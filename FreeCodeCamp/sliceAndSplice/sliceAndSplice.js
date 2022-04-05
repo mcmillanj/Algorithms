6b93fd400d64be277031f431e159e1bca155870f
June Mcmillan <mcmillanj0106@yahoo.com>
2022-04-05 11:28:16 -0400

    Update sliceAndSplice.js

--- FreeCodeCamp/sliceAndSplice/sliceAndSplice.js
+++ FreeCodeCamp/sliceAndSplice/sliceAndSplice.js
@@ -1,3 +1,11 @@
+
+const arr1 = [[1,2,3],[4,5,6],[7,8,9]];
+const arr2 = [[1,2],["a","b"],[7,8]];
+const arr3 =["head", "shoulders", "claw", "tentacle", "knees", "toes","feet","eyes","tongue"];
+// let n =3;
+let copy1 = [];
+let copy2 = [];
+
 function frankenSplice(arr1, arr2, n) {
   const copy1 = arr1.slice();
   const copy2 = arr2.slice();
@@ -5,9 +13,9 @@ function frankenSplice(arr1, arr2, n) {
   return copy2;
 }
 
-console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1)); //Should return [4,1,2,3,5,6]
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 2)); //Should return [4,5,1,2,3,6]
 console.log(frankenSplice([1, 2], ['a', 'b'], 1)); //Should return ["a", 1, 2, "b"]
-console.log(
+
   //Should return
-  frankenSplice(['claw', 'tentacle'], ['head', 'shoulders', 'knees', 'toes'], 2)
-); //Should return ["head", "shoulders", "claw", "tentacle", "knees", "toes"]
+ console.log(frankenSplice(["head", "shoulders", "claw","tentacle"], ['head', 'shoulders', 'knees', "toes","feet","eyes","tongue"], 2));)
+); //Should return ["head","shoulders","head","shoulders","claw","tentacle","knees","toes","feet","eyes","tongue"]
